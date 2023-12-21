875e52710e28f154cda0a27300485b29dadd3615
Francesco Noacco <francesco.noacco2000@gmail.com>
2023-12-21 11:51:36 +0100

    feat: use otp packages for elixir-ls

--- lib/beamOverlay.nix
+++ lib/beamOverlay.nix
@@ -9,12 +9,15 @@ let
   beamAttrSet = if wxSupport then prev.beam else prev.beam_nox;
 
   customElixirOTP = beamPackage { name = "erlang"; version = elixir-otp; parentAttrSet = beamAttrSet.interpreters; pkgs = prev; };
+  otpPackages = beamAttrSet.packagesWith customElixirOTP;
+
   customErlang = beamPackage { name = "erlang"; version = erlang; parentAttrSet = beamAttrSet.interpreters; pkgs = prev; };
-  customElixir = beamPackage { name = "elixir"; version = elixir; parentAttrSet = (beamAttrSet.packagesWith customElixirOTP); pkgs = prev; };
+  customElixir = beamPackage { name = "elixir"; version = elixir; parentAttrSet = otpPackages; pkgs = prev; };
+  customElixirLS = otpPackages.elixir-ls.override { elixir = customElixir; };
 in
 {
   inherit customElixir customErlang customElixirOTP;
-  elixir-ls = (prev.elixir-ls.override { elixir = customElixir; });
+  elixir-ls = customElixirLS;
   elixirDevShell = final.callPackage ./elixirDevShell.nix { erlang = customErlang; elixir = customElixir; };
   customMixRelease = final.callPackage ./mixRelease.nix { inherit customElixir customElixirOTP; };
 }

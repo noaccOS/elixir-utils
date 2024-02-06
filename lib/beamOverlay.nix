fa5b4261fd2c11b7d7ddfcb136eff139a95872a1
Francesco Noacco <francesco.noacco2000@gmail.com>
2024-02-06 18:56:12 +0100

    fix(elixir-ls): build failing for elixir >= 1.16
    Signed-off-by: Francesco Noacco <francesco.noacco2000@gmail.com>

--- lib/beamOverlay.nix
+++ lib/beamOverlay.nix
@@ -13,7 +13,17 @@ let
 
   customErlang = beamPackage { name = "erlang"; version = erlang; parentAttrSet = beamAttrSet.interpreters; pkgs = prev; };
   customElixir = beamPackage { name = "elixir"; version = elixir; parentAttrSet = otpPackages; pkgs = prev; };
-  customElixirLS = otpPackages.elixir-ls.override { elixir = customElixir; };
+  customElixirLS = (otpPackages.elixir-ls.override { elixir = customElixir; }).overrideAttrs (old: {
+    buildPhase =
+      # Elixir 1.16.0 or newer
+      if (builtins.compareVersions customElixir.version "1.16.0" != -1)
+      then ''
+        runHook preBuild
+        mix do compile --no-deps-check, elixir_ls.release2
+        runHook postBuild
+      ''
+      else old.buildPhase;
+  });
 in
 {
   inherit customElixir customErlang customElixirOTP;

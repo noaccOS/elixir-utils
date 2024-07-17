219ab3b9f527855232fb0a80f41b8239f0a949cf
Francesco Noacco <francesco.noacco2000@gmail.com>
2024-07-17 09:58:43 +0200

    feat: add adsfDevShell

--- lib.nix
+++ lib.nix
@@ -1,21 +1,35 @@
-lib:
-allSystems:
-defaultSystems:
-rec {
-  inherit allSystems defaultSystems;
+lib: rec {
   asdfOverlay = import lib/asdfOverlay.nix lib;
   beamOverlay = import lib/beamOverlay.nix;
   asdfPakcages = import lib/asdfPakcages.nix lib;
   beamPackages = import lib/beamPackages.nix;
 
-  devShell = { pkgs, lsp ? pkgs.elixir-ls, erlang ? null, elixir ? null, elixir-otp ? erlang, wxSupport ? true }:
+  devShell =
+    {
+      pkgs,
+      lsp ? pkgs.elixir-ls,
+      erlang ? null,
+      elixir ? null,
+      wxSupport ? true,
+    }:
     let
-      overlay = beamOverlay { inherit erlang elixir elixir-otp wxSupport; };
+      overlay = beamOverlay { inherit erlang elixir wxSupport; };
       finalPkgs = pkgs.extend overlay;
     in
-    finalPkgs.callPackage lib/elixirDevShell.nix {
-      inherit lsp;
-      erlang = finalPkgs.customErlang;
-      elixir = finalPkgs.customElixir;
+    finalPkgs.callPackage lib/elixirDevShell.nix { inherit lsp; };
+
+  asdfDevShell =
+    {
+      pkgs,
+      src,
+      lsp ? pkgs.elixir-ls,
+      wxSupport ? true,
+    }:
+    let
+      asdf = import ./parseASDF.nix lib (src + "/.tool-versions");
+    in
+    pkgs.callPackage devShell {
+      inherit lsp wxSupport;
+      inherit (asdf) elixir erlang;
     };
 }

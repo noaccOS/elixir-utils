5f7c3579c3e63464f5c1ee6f880886b917a9ee98
Francesco Noacco <francesco.noacco2000@gmail.com>
2024-07-17 09:55:14 +0200

    refactor!: use flake-parts

--- flake.nix
+++ flake.nix
@@ -1,39 +1,38 @@
 {
   description = "Utilities to setup an elixir project with nix";
-  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.systems.url = "github:nix-systems/default";
 
-  outputs = { self, nixpkgs, flake-utils }:
-  let
-    allSystems = nixpkgs.lib.platforms.unix;
-    defaultSystems = nixpkgs.lib.lists.intersectLists allSystems flake-utils.lib.defaultSystems;
-  in
-  (
-    {
-      lib = import ./lib.nix nixpkgs.lib allSystems defaultSystems;
-      templates = {
-        default = self.templates.simple;
-        simple = {
-          path = examples/simple;
-          description = "Simple elixir shell with default elixir version";
-        };
-        custom = {
-          path = examples/custom;
-          description = "Elixir shell with custom elixir version";
-        };
-        asdf = {
-          path = examples/asdf;
-          description = "Elixir shell with elixir version from .tool-versions";
-        };
+  outputs =
+  inputs:
+  inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = import inputs.systems;
+    flake.lib = import ./lib.nix inputs.nixpkgs.lib;
+    flake.templates = {
+      default = inputs.self.templates.simple;
+      simple = {
+        path = examples/simple;
+        description = "Simple elixir shell with default elixir version";
       };
-    } //
-      flake-utils.lib.eachSystem defaultSystems (system:
-        let pkgs = import nixpkgs { inherit system; }; in
-        {
-          formatter = pkgs.nixpkgs-fmt;
-          packages.updateRefs = pkgs.callPackage ./util/update_sha/default.nix { };
-          devShells.default = self.lib.devShell { inherit pkgs; };
-          devShells.latest = self.lib.devShell { inherit pkgs; erlang = "27"; elixir = "1.16"; };
-        })
-    );
+      custom = {
+        path = examples/custom;
+        description = "Elixir shell with custom elixir version";
+      };
+      asdf = {
+        path = examples/asdf;
+        description = "Elixir shell with elixir version from .tool-versions";
+      };
+    };
+
+    perSystem = { self', pkgs, ... }: {
+      formatter = pkgs.nixfmt-rfc-style;
+      packages.updateRefs = pkgs.callPackage ./util/update_sha/default.nix { };
+      devShells.default = pkgs.callPackage self'.lib.devShell { };
+      devShells.latest = pkgs.callPackage self'.lib.devShell {
+        erlang = "27";
+        elixir = "1.16";
+      };
+    };
+  };
 }
